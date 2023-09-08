bf069de79349dcdef1d00cacc10196608f0655a3
Cardenascg <cardenas.g1127@gmail.com>
2023-09-08 16:41:57 -0600

    fin proyecto seccion 8 curso de angular

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
+
+
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList:Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey:string = '6vgLfPSDT4VkMQeY2q45JgcM3EG2sNrZ';
+  private serviceURL:string = 'https://api.giphy.com/v1/gifs';
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -19,6 +28,23 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history',JSON.stringify(this._tagsHistory));
+    // localStorage.setItem('gifs', JSON.stringify(this.gifList));
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history'))  return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if(this._tagsHistory.length  === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+
+
   }
 
   searchTag(tag:string):void {
@@ -26,6 +52,16 @@ export class GifsService {
 
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+    .set('api_key',this.apiKey)
+    .set('q',tag)
+    .set('limit','10');
+
+    this.http.get<SearchResponse>(`${this.serviceURL}/search`,{params})
+      .subscribe(resp => {
+        this.gifList = resp.data;
+        // console.log({gifs: this.gifList});
+      })
   }
 
 }
